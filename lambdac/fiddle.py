576c29047b37f372f3f3a4ad6c2fdfba2600728a
Marcos Modenesi <marcosmodenesi@gmail.com>
2019-05-11 12:13:27 -0300

    Code style on fiddle

--- lambdac/fiddle.py
+++ lambdac/fiddle.py
@@ -1,14 +1,95 @@
 from lambdac import *
 from replace import *
 
-TRUE = Abstraction(Var('x'), Abstraction(Var('y'), Var('x')))
-FALSE = Abstraction(Var('x'), Abstraction(Var('y'), Var('y')))
-NOT = Abstraction(Var('x'), Application(Application(Var('x'), FALSE), TRUE))
-OR = Abstraction(Var('x'), Abstraction(Var('y'), Application(Application(Var('x'), Var('x')), Var('y'))))
-AND = Abstraction(Var('x'), Abstraction(Var('y'), Application(Application(Var('x'), Var('y')), Var('x'))))
-IMPLIES = Abstraction(Var('x'), Abstraction(Var('y'), Application(Application(OR, Application(NOT, Var('x'))), Var('y'))))
-EQUIV = Abstraction(Var('x'), Abstraction(Var('y'), Application(Application(AND, Application(Application(IMPLIES, Var('x')), Var('y'))), Application(Application(IMPLIES, Var('y')), Var('x')))))
+# boolean constants
+TRUE = Abstraction(
+    Var('x'),
+    Abstraction(
+        Var('y'),
+        Var('x')))
 
+FALSE = Abstraction(
+    Var('x'),
+    Abstraction(
+        Var('y'),
+        Var('y')))
 
-ZERO = Abstraction(Var('f'), Abstraction(Var('x'), Var('x')))
-SUCC = Abstraction(Var('n'), Abstraction(Var('f'), Abstraction(Var('x'), Application(Var('f'), Application(Application(Var('n'), Var('f')), Var('x'))))))
+# boolean operations
+NOT = Abstraction(
+    Var('x'),
+    Application(
+        Application(
+            Var('x'),
+            FALSE),
+        TRUE))
+
+OR = Abstraction(
+    Var('x'),
+    Abstraction(
+        Var('y'),
+        Application(
+            Application(
+                Var('x'),
+                Var('x')),
+            Var('y'))))
+
+AND = Abstraction(
+    Var('x'),
+    Abstraction(
+        Var('y'),
+        Application(
+            Application(
+                Var('x'),
+                Var('y')),
+            Var('x'))))
+
+IMPLIES = Abstraction(
+    Var('x'),
+    Abstraction(
+        Var('y'),
+        Application(
+            Application(
+                OR,
+                Application(
+                    NOT,
+                    Var('x'))),
+                Var('y'))))
+
+EQUIV = Abstraction(
+    Var('x'),
+    Abstraction(
+        Var('y'),
+        Application(
+            Application(
+                AND,
+                Application(
+                    Application(
+                        IMPLIES,
+                        Var('x')),
+                    Var('y'))),
+                Application(
+                    Application(
+                        IMPLIES,
+                        Var('y')),
+                    Var('x')))))
+
+# numbers
+ZERO = Abstraction(
+    Var('f'),
+    Abstraction(
+        Var('x'),
+        Var('x')))
+
+SUCC = Abstraction(
+    Var('n'),
+    Abstraction(
+        Var('f'),
+        Abstraction(
+            Var('x'),
+            Application(
+                Var('f'),
+                Application(
+                    Application(
+                        Var('n'),
+                        Var('f')),
+                    Var('x'))))))
